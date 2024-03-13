09f33829f82c384216704cdcc2f1434b984bc736
adrian.proczek <adrian.proczek@gmail.com>
2024-03-13 17:27:01 +0100

    Initial commit

--- main.py
+++ main.py
@@ -35,15 +35,11 @@ while game_is_on:
     if snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.xcor() > 280 or snake.head.ycor() > 280:
         game_is_on = False
         score.game_over()
-        # score.reset()
-        # snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
             score.game_over()
-            # score.reset()
-            # snake.reset()
 
 
 screen.exitonclick()
